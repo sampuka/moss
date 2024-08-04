537c1040c66e744e7b52819203177a66ba24f39c
Mathias Ebbensgaard Jensen <matddi@gmail.com>
2024-08-04 13:55:14 +0200

    Improved display settings

--- src/application/display_settings.cpp
+++ src/application/display_settings.cpp
@@ -4,9 +4,16 @@
 
 #include "stdio.h"
 
+extern "C" {
+int uefi_conin_readkeystroke(uint16_t* c);
+int uefi_conin_reset();
+}
+
 void display_settings()
 {
     bool finished = false;
+
+    uefi_conin_reset();
 
     while (!finished)
     {
@@ -36,7 +43,7 @@ void display_settings()
 
             char text[50];
 
-            snprintf(text, 50, " %2d: %4dx%-4d", display_index, v.hoz_res, v.ver_res);
+            snprintf(text, sizeof text, " %2d: %4dx%-4d", display_index, v.hoz_res, v.ver_res);
 
             if (display_index == set.active_mode)
             {
@@ -44,6 +51,27 @@ void display_settings()
             }
 
             screen_render_text(10, 30 + 20 * (display_index - low_index), text);
+        }
+
+        uint16_t key = 0;
+        if (uefi_conin_readkeystroke(&key) == 0)
+        {
+            uefi_conin_reset();
+
+            int next = set.active_mode;
+
+            if ((key == 97) && (set.active_mode < (set.video_mode_size - 1)))
+            {
+                next += 1;
+            }
+            else if ((key == 122) && (set.active_mode > 0))
+            {
+                next -= 1;
+            }
+
+            VideoMode next_mode = set.video_modes[next];
+
+            screen_set_resolution(next_mode.hoz_res, next_mode.ver_res);
         }
 
         screen_draw_frame();
